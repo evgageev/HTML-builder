f49fe26ea9c944bce35af8725c704b3857c458ef
evgageev <evgageev98@gmail.com>
2023-05-04 13:00:47 +0500

    feat: solve 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const { stdout } = process;
+
+const writableStream = fs.createWriteStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8'
+);
+
+stdout.write('Hello!\n');
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    rl.close();
+  }
+  writableStream.write(`${line}\n`);
+});
+
+rl.once('close', () => {
+  stdout.write('See you later!');
+});
