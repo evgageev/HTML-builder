d9517c1374255c20ddaf8f9fa2cb4a96e755484a
evgageev <evgageev98@gmail.com>
2023-05-05 10:20:13 +0500

    feat: sovle 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(
+  path.join(__dirname, 'files'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(__dirname, 'files', file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          () => {
+            if (err) throw err;
+            console.log(`${file.name} was coppied`);
+          }
+        );
+      });
+    }
+  }
+);
